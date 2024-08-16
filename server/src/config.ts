200d1fbdf0e74cff59655ed53c10053ebfe3996f
Josh Layton <josh.l8n@gmail.com>
2024-08-15 20:14:59 -0600

    test: deployment build env vars / CORS

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,18 +1,15 @@
 import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables
+dotenv.config(); // load environment variables
 
-const PG_USER = process.env.PG_USER;
-const PG_PASSWORD = process.env.PG_PASSWORD;
-const PG_HOST = process.env.PG_HOST;
-const PG_DB = process.env.PG_DB;
+const { PG_USER, PG_PASSWORD, PG_HOST, PG_DB, CORS_DOMAINS } = process.env;
 
 export const config = {
   postgres: {
     connectionString: `postgres://${PG_USER}:${PG_PASSWORD}@${PG_HOST}/${PG_DB}`,
   },
   cors: {
-    origin: ['http://localhost:9000'],
+    origin: CORS_DOMAINS ? CORS_DOMAINS.split(',') : false,  // get list of domains from env
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 };
